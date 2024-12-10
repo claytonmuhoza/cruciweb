e312f81fd26f102d0955e343546bd4359aafe82a
muhozaclayton <muhozaclayton@gmail.com>
2024-12-10 01:04:42 +0100

    some change

--- Config/Database.php
+++ Config/Database.php
@@ -4,7 +4,7 @@ return [
     'host' => 'localhost',
     'dbname' => 'mots_croises',
     'username' => 'root',
-    'password' => 'abagabo',
+    'password' => '',
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
